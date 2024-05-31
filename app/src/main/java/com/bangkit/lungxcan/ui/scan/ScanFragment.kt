04948f5172cdefcf79f60f52ba013cbd5e3ad66f
izzaar_ <izzaar1512@gmail.com>
2024-05-31 19:13:35 +0700

    feat: Implement conditional analyze button & scan feature backend stubs

--- app/src/main/java/com/bangkit/lungxcan/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/bangkit/lungxcan/ui/scan/ScanFragment.kt
@@ -24,17 +24,52 @@ class ScanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val scanViewModel =
-            ViewModelProvider(this).get(ScanViewModel::class.java)
+            ViewModelProvider(this)[ScanViewModel::class.java]
 
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //val textView: TextView = binding.textNotifications
-//        scanViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
         binding.appBar.topAppBar.title = getString(R.string.title_scan)
+
+        binding.btnAnalyze.isEnabled = false
+
+        binding.apply {
+            btnCamera.setOnClickListener {
+                startGallery()
+                updateAnalyzeButtonState()
+            }
+            btnGallery.setOnClickListener {
+                startCamera()
+                updateAnalyzeButtonState()
+            }
+            btnAnalyze.setOnClickListener { analyzeImage() }
+
+        }
         return root
+    }
+
+    private fun startGallery() {
+        // TODO: Get image from Gallery.
+    }
+
+    private fun startCamera() {
+        // TODO: Get image from Camera.
+    }
+
+    private fun showImage() {
+        // TODO: Show image based on what selected from gallery/camera.
+    }
+
+    private fun analyzeImage() {
+        // TODO: Analyzing chosen image.
+    }
+
+    private fun updateAnalyzeButtonState() {
+        binding.apply {
+            btnAnalyze.isEnabled = true
+            btnAnalyze.setBackgroundColor(resources.getColor(R.color.green))
+            btnAnalyze.setTextColor(resources.getColor(R.color.white))
+        }
     }
 
     override fun onDestroyView() {
