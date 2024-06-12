1cc24a25adcdd4693538d0c83725c571a42ef24b
izzaar_ <izzaar1512@gmail.com>
2024-06-12 19:25:29 +0700

    feat: fix scan layout and implement history feature layout with dummy data

---
+++ app/src/main/java/com/bangkit/lungxcan/data/DummyHistory.kt
@@ -0,0 +1,7 @@
+package com.bangkit.lungxcan.data
+
+data class DummyHistory(
+    val prob: Int,
+    val diseases: String,
+    val date: String
+)
--- app/src/main/java/com/bangkit/lungxcan/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/bangkit/lungxcan/ui/history/HistoryFragment.kt
@@ -7,16 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bangkit.lungxcan.R
+import com.bangkit.lungxcan.data.DummyHistory
 import com.bangkit.lungxcan.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
 
-    private var _binding: FragmentHistoryBinding? = null
+    private lateinit var _binding: FragmentHistoryBinding
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,19 +29,39 @@ class HistoryFragment : Fragment() {
             ViewModelProvider(this)[HistoryViewModel::class.java]
 
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        historyViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textDashboard
+//        historyViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        val layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvHistory.layoutManager = layoutManager
 
         binding.appBar.topAppBar.title = getString(R.string.title_history)
-        return root
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val histories = mutableListOf<DummyHistory>()
+//        for (i in 0..10) {
+//            val data = DummyHistory(80, "Lung Cancer", "24 Juni 2024")
+//            histories.add(data)
+//        }
+
+        if (histories.isEmpty()) {
+            binding.rvHistory.visibility = View.GONE
+            binding.tvEmptyText.visibility = View.VISIBLE
+        } else {
+            val historyAdapter = HistoryAdapter()
+            historyAdapter.submitList(histories)
+            binding.rvHistory.adapter = historyAdapter
+        }
     }
+
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 }
