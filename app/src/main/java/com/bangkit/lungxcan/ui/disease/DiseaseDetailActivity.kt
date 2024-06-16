4995440c94c21381a5dffe4a88ad265024c15a4c
izzaar_ <izzaar1512@gmail.com>
2024-06-16 23:43:53 +0700

    feat: add disease info detail activity

---
+++ app/src/main/java/com/bangkit/lungxcan/ui/disease/DiseaseDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.bangkit.lungxcan.ui.disease
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bangkit.lungxcan.R
+
+class DiseaseDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_disease_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
