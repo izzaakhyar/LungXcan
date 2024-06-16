d2a625bd91406ad489de4825294d1a3e4bb9fa99
izzaar_ <izzaar1512@gmail.com>
2024-06-16 22:44:41 +0700

    refactor: change history feature to disease info

--- app/src/main/java/com/bangkit/lungxcan/ui/disease/DiseaseFragment.kt
+++ app/src/main/java/com/bangkit/lungxcan/ui/disease/DiseaseFragment.kt
@@ -1,20 +1,19 @@
-package com.bangkit.lungxcan.ui.history
+package com.bangkit.lungxcan.ui.disease
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bangkit.lungxcan.R
-import com.bangkit.lungxcan.data.DummyHistory
-import com.bangkit.lungxcan.databinding.FragmentHistoryBinding
+import com.bangkit.lungxcan.data.DummyDisease
+import com.bangkit.lungxcan.databinding.FragmentDiseaseBinding
 
-class HistoryFragment : Fragment() {
+class DiseaseFragment : Fragment() {
 
-    private lateinit var _binding: FragmentHistoryBinding
+    private lateinit var _binding: FragmentDiseaseBinding
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,38 +24,38 @@ class HistoryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val historyViewModel =
-            ViewModelProvider(this)[HistoryViewModel::class.java]
+        val diseaseViewModel =
+            ViewModelProvider(this)[DiseaseViewModel::class.java]
 
-        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        _binding = FragmentDiseaseBinding.inflate(inflater, container, false)
 
 //        val textView: TextView = binding.textDashboard
 //        historyViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
-        val layoutManager = LinearLayoutManager(requireActivity())
+        val layoutManager = GridLayoutManager(requireActivity(), 2)
         binding.rvHistory.layoutManager = layoutManager
 
-        binding.appBar.topAppBar.title = getString(R.string.title_history)
+        binding.appBar.topAppBar.title = getString(R.string.title_disease)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val histories = mutableListOf<DummyHistory>()
-//        for (i in 0..10) {
-//            val data = DummyHistory(80, "Lung Cancer", "24 Juni 2024")
-//            histories.add(data)
-//        }
+        val histories = mutableListOf<DummyDisease>()
+        for (i in 0..9) {
+            val data = DummyDisease(80, "Lung Cancer", "24 Juni 2024")
+            histories.add(data)
+        }
 
         if (histories.isEmpty()) {
             binding.rvHistory.visibility = View.GONE
             binding.tvEmptyText.visibility = View.VISIBLE
         } else {
-            val historyAdapter = HistoryAdapter()
-            historyAdapter.submitList(histories)
-            binding.rvHistory.adapter = historyAdapter
+            val diseaseAdapter = DiseaseAdapter()
+            diseaseAdapter.submitList(histories)
+            binding.rvHistory.adapter = diseaseAdapter
         }
     }
 
